6cba2c1fa2a21b65d4b0943391e5ad33cb4b1160
Manuel Fernández-Barba <107534254+ManuFBarba@users.noreply.github.com>
2023-07-24 10:54:51 +0200

    UPDATED CODE

--- Averaged_MHW_Metrics.py
+++ Averaged_MHW_Metrics.py
@@ -1,6 +1,6 @@
 ############################# Averaged SIC and N-SAT #######################
 
-#Loading requires modeles
+#Loading requires modules
 import numpy as np
 import xarray as xr
 import pandas as pd
